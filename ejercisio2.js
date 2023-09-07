7108d50c28eba5dad2261032dbdf60c9ab077dc0
Cristian camilo acuña barajas <119276208+Cristiaan1024@users.noreply.github.com>
2023-09-07 08:39:43 -0500

    Add files via upload

---
+++ ejercisio2.js
@@ -0,0 +1,11 @@
+var sueldo; 
+var bonifi;
+var nom;
+
+sueldo = prompt
+bonifi = prompt
+nom = prompt
+
+a = sueldo + bonifi 
+
+alert ("Empleado") + nom ("su sueldo anual con su bonificacion fija es:") + a ;
